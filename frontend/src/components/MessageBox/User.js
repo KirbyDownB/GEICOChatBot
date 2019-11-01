0305671b68a517c86e56a565bf6cae995bf5b714
John Shin <jshin029@ucr.edu>
2019-11-01 15:29:19 -0700

    added dates

--- frontend/src/components/MessageBox/User.js
+++ frontend/src/components/MessageBox/User.js
@@ -4,12 +4,34 @@ import './User.css';
 const logo = require('../../assets/userLogo.svg');
 
 class User extends Component {
+  state = {
+    timeStamp: ""
+  }
+
+  formatAMPM = (date) => {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
+
+  componentDidMount = () => {
+    let date = new Date();
+    this.setState({
+      timeStamp: this.formatAMPM(date)
+    })
+  }
+
   render(){
     return(
       <div className="user__container">
         <img className="user__img" src={logo} alt="user"></img>
         <div className="user__text-wrapper">
-          <div className="user__name">User</div>
+          <div className="user__name">User {this.state.timeStamp}</div>
           <div className="user__text">{this.props.text}</div>
         </div>
       </div>
