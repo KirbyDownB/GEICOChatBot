51a49c424f1e4975d38656bf91be9d3aedc868b9
William Shiao <willshiao@gmail.com>
2019-11-08 15:18:17 -0800

    Fix syntax error

--- backend/library.py
+++ backend/library.py
@@ -1,13 +1,12 @@
 TABLE = {
     'day': {"text": "Ok {}, how has your day been? This will give us some idea about what movies to \
             recommend you.", "type": "bot", "question": "day", "topic": "normal"},
-    'bother_user': {"text": "Enough with the shananigans. Type in \"recommend movies\" or \"recommend songs\" to get started.", "type": "bot", "topic": "normal", "question": "general"}
+    'bother_user': {"text": "Enough with the shananigans. Type in \"recommend movies\" or \"recommend songs\" to get started.", "type": "bot", "topic": "normal", "question": "general"},
     'music_prompt': {"text": "Give me a song that you like and I'll try to find some similar ones!", "type": "bot", "question": "music_prompt", "topic": "normal"},
     'music_prompt': {"text": "Give me a song that you like and I'll try to find some similar ones!", "type": "bot", "question": "suggest_music", "topic": "normal"},
     'movie_response': {"text": "{} What are some of your favorite movies? Separate each one with a \
             comma.", "type": "bot", "question": "favorite_movies", "topic": "normal"}
 }
-
 
 def fetch_response(question, username=None):
     if question not in TABLE:
