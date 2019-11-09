09aa95006338445f7ec6dcc97812244c602f3bd2
ericong18 <ericong18@gmail.com>
2019-11-08 22:18:21 -0800

    Delete movie

--- frontend/src/components/Saved/Saved.js
+++ frontend/src/components/Saved/Saved.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Saved.css';
-import { BASE_URL, tokenKeyName } from '../../constants';
+import { BASE_URL, tokenKeyName, showMessage, REQUEST_ERROR } from '../../constants';
 import { Icon, Spin, Tooltip } from 'antd';
 
 const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
@@ -42,6 +42,59 @@ class Saved extends Component {
     }
   }
 
+  handleDeleteSavedMovie = (e, imdbID) => {
+    e.preventDefault();
+    const token = localStorage.getItem(tokenKeyName);
+
+    if (token) {
+      fetch(`${BASE_URL}/api/delete_movie`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + token
+        },
+        body: JSON.stringify({
+          "imdbID": imdbID
+        })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log("Got data after deleting movie", data);
+        })
+        .catch(error => {
+          console.error(error);
+          showMessage(REQUEST_ERROR);
+        })
+    }
+  }
+
+  handleDeleteSavedMusic = (e, spotifyID) => {
+    e.preventDefault();
+    const token = localStorage.getItem(tokenKeyName);
+
+    if (token) {
+      console.log("Deleting spotify id", spotifyID)
+      fetch(`${BASE_URL}/api/delete_song`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + token
+        },
+        body: JSON.stringify({
+          "songID": spotifyID
+        })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log("Got data after deleting movie", data);
+        })
+        .catch(error => {
+          console.error(error);
+          showMessage(REQUEST_ERROR);
+        })
+    }
+  }
+
   render() {
     return (
       <div className="saved__container">
@@ -54,12 +107,19 @@ class Saved extends Component {
             {this.state.movies.length > 0 && <div className="saved__movies--container">
               <div className="saved__movies--header">Saved Movies</div>
               <div className="row">
-                {this.state.movies.map(movie => {
+                {this.state.movies.map(({ imdbID, Title, Poster }) => {
+                  const imdbURL = `https://www.imdb.com/title/${imdbID}/`;
+
                   return (
-                    <div className="col-4">
-                      <Tooltip title={movie.Title}>
-                      	<img src={movie.Poster} alt="" className="saved__moviePoster"/>
-                      </Tooltip>
+                    <div className="col-4 saved__movie">
+                      <a href={imdbURL} target="_blank" rel="noopener noreferrer">
+                        <Tooltip title={Title}>
+                          <img src={Poster} alt="" className="saved__moviePoster"/>
+                        </Tooltip>
+                      </a>
+                      <div className="remove" onClick={e => this.handleDeleteSavedMovie(e, imdbID)}>
+                        <Icon type="close-circle" theme="filled" style={{ fontSize: "20px" }} />
+                      </div>
                     </div>
                   )
                 })}
@@ -68,12 +128,17 @@ class Saved extends Component {
             {this.state.music.length > 0 && <div className="music--container">
               <div className="saved__music--header">Saved Music</div>
               <div className="row">
-                {this.state.music.map(({ album, name }) => {
+                {this.state.music.map(({ album, name, external_urls: { spotify: spotifyURL }, id: spotifyID }) => {
                   return (
-                    <div className="col-4">
-                      <Tooltip title={name}>
-                        <img src={album.images[0].url} alt="" className="saved__musicAlbum"/>
-                      </Tooltip>
+                    <div className="col-4 saved__music">
+                      <a href={spotifyURL} target="_blank" rel="noopener noreferrer">
+                        <Tooltip title={name}>
+                          <img src={album.images[0].url} alt="" className="saved__musicAlbum"/>
+                        </Tooltip>
+                      </a>
+                      <div className="remove" onClick={e => this.handleDeleteSavedMusic(e, spotifyID)}>
+                        <Icon type="close-circle" theme="filled" style={{ fontSize: "20px" }} />
+                      </div>
                     </div>
                   )
                 })}
